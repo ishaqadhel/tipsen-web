838c9995f5f129d0056bde7f67ec4a2fd7bd3ba4
ishaqadhel <ishaq.adhel@gmail.com>
2024-03-30 11:50:49 +0700

    feat: add bootstrap file for app

---
+++ src/bootstrap.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const Bootstrap: React.FC = () => {
+    return (
+        <div>
+            <p className='text-red-500'>react app</p>
+        </div>
+    );
+};
+
+export default Bootstrap;
--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 
 import '@/index.css';
 
-import App from '@/App';
 import reportWebVitals from '@/reportWebVitals';
+import Bootstrap from '@/bootstrap';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement,
 );
 root.render(
     <React.StrictMode>
-        <App />
+        <Bootstrap />
     </React.StrictMode>,
 );
 
